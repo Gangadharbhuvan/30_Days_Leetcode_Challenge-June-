ad8d696bb6191390f3a70388d62995beac37595d
Gangadhar DN <44730031+Gangadharbhuvan@users.noreply.github.com>
2020-06-20 18:41:18 +0530

    Added Day-20 Solution

---
+++ Day-20_Permutation_Sequence.py
@@ -0,0 +1,39 @@
+'''
+	The set [1,2,3,...,n] contains a total of n! unique permutations.
+
+By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+
+"123"
+"132"
+"213"
+"231"
+"312"
+"321"
+Given n and k, return the kth permutation sequence.
+
+Note:
+
+Given n will be between 1 and 9 inclusive.
+Given k will be between 1 and n! inclusive.
+Example 1:
+
+Input: n = 3, k = 3
+Output: "213"
+Example 2:
+
+Input: n = 4, k = 9
+Output: "2314"
+
+'''
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        f, nums = [1], [0]
+        for i in range(1, n + 1):
+            f.append(f[i - 1] * i)
+            nums.append(i)
+        res = []
+        for i in range(n - 1, -1, -1):
+            idx = math.ceil(k / f[i])
+            res.append(nums.pop(idx))
+            k -= (idx - 1) * f[i]
+        return ''.join(map(str, res))
