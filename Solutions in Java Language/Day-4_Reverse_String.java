34f8a4e8e1d530a3d00fc353fd74d1c241dae306
Gangadhar DN <44730031+Gangadharbhuvan@users.noreply.github.com>
2020-07-21 11:49:52 +0530

    Solutions in Java Language

---
+++ Solutions in Java Language/Day-4_Reverse_String.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+        while (start < end) {
+            swap(s, start, end);
+            start++;
+            end--;
+        }
+        
+    }
+    
+    private void swap(char[] s, int i, int j) {
+        char t = s[i];
+        s[i] = s[j];
+        s[j] = t;
+    }
+}
